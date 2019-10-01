a0ed8421670a9a2e0169505780e59da6c35624f1
kingshkumar <56027852+kingshkumar@users.noreply.github.com>
2019-10-01 17:17:34 +0530

    Add files via upload

---
+++ image_detection.py
@@ -0,0 +1,10 @@
+import cv2
+image=cv2.imread("C:/Users/Kanishk/OneDrive/Pictures/image_human.jpg")
+#image=image.resize(image,1.3)
+cascade=cv2.CascadeClassifier('FaceFront.xml')
+faces=cascade.detectMultiScale(image,1.3)
+print(faces)
+for x,y,w,h in faces:
+    cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+    
+cv2.imshow('result',image)
